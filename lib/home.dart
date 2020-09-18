27aa4ba8f538687b86465b9da121ffdf9092f95c
Amey Sunu <ameysunu00719@gmail.com>
2020-09-18 23:52:09 +0300

    home screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final nameController = TextEditingController();
 
 class Home extends StatefulWidget {
   @override
@@ -37,33 +40,122 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  "Welcome, let's set some stuff right before you start:",
-                  style: TextStyle(fontFamily: 'Gotham', fontSize: 20),
+      backgroundColor: Colors.white,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/wallpaper.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    "Welcome, let's set some stuff right before you start:",
+                    style: TextStyle(fontFamily: 'Gotham', fontSize: 20),
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton<ListItem>(
-                      style:
-                          TextStyle(fontFamily: 'Gotham', color: Colors.black),
-                      value: _selectedItem,
-                      items: _dropdownMenuItems,
-                      onChanged: (value) {
-                        setState(() {
-                          _selectedItem = value;
-                        });
-                      }),
+                Card(
+                  color: Hexcolor('#FFDFCA'),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("images/game.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    width: MediaQuery.of(context).size.width * 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            "Choose Player Mode:",
+                            style:
+                                TextStyle(fontFamily: 'Gotham', fontSize: 18),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10.0),
+                          child: DropdownButtonHideUnderline(
+                            child: DropdownButton<ListItem>(
+                                style: TextStyle(
+                                    fontFamily: 'Gotham', color: Colors.black),
+                                value: _selectedItem,
+                                items: _dropdownMenuItems,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _selectedItem = value;
+                                  });
+                                }),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            "Your gaming name",
+                            style:
+                                TextStyle(fontFamily: 'Gotham', fontSize: 18),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 0, 10, 0),
+                          child: TextFormField(
+                            controller: nameController,
+                            style: TextStyle(
+                                color: Colors.black, fontFamily: 'Gotham'),
+                            decoration: new InputDecoration(
+                              enabledBorder: new OutlineInputBorder(
+                                  borderSide:
+                                      new BorderSide(color: Colors.black)),
+                              hintStyle: TextStyle(
+                                fontFamily: 'Gotham',
+                                color: Colors.black54,
+                                fontSize: 15,
+                                fontStyle: FontStyle.italic,
+                              ),
+                              labelStyle: TextStyle(
+                                  fontFamily: 'Gotham', color: Colors.white),
+                              hintText: 'Ex: KnightSlayer, DungeonHunter',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    "Only for Multiplayer users",
+                    style: TextStyle(
+                        fontFamily: 'Gotham',
+                        fontSize: 15,
+                        color: Hexcolor('#5F5C6D')),
+                  ),
+                ),
+                Card(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 1,
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    child: Column(
+                      children: [
+                        Text(
+                          "Enter team ID",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
