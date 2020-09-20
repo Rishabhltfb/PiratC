13900d7a6706dca426fc7ce83702cb9617a77ef7
Amey Sunu <ameysunu00719@gmail.com>
2020-09-20 12:33:41 +0300

    bug fixes

--- lib/agora/src/pages/index.dart
+++ lib/agora/src/pages/index.dart
@@ -57,99 +57,101 @@ class IndexState extends State<IndexPage> {
           ),
           padding: const EdgeInsets.symmetric(horizontal: 20),
           height: 400,
-          child: Column(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Expanded(
-                      child: TextField(
-                    style: TextStyle(
-                        color: Hexcolor('#5C6178'), fontFamily: 'Gotham'),
-                    controller: _channelController,
-                    decoration: InputDecoration(
-                      labelStyle: TextStyle(
-                          color: Hexcolor('#5C6178'), fontFamily: 'Gotham'),
-                      hintStyle: TextStyle(
-                          color: Hexcolor('#5C6178'), fontFamily: 'Gotham'),
-                      errorStyle:
-                          TextStyle(color: Colors.red, fontFamily: 'Gotham'),
-                      errorText: _validateError ? 'ID is mandatory' : null,
-                      border: UnderlineInputBorder(
-                        borderSide: BorderSide(width: 1),
-                      ),
-                      hintText: 'Enter Team ID',
-                    ),
-                  ))
-                ],
-              ),
-              Column(
-                children: [
-                  ListTile(
-                    //title: Text(ClientRole.Broadcaster.toString()),
-                    title: Text(
-                      "Start a call",
-                      style: TextStyle(
-                          fontFamily: 'Gotham', color: Hexcolor('#5C6178')),
-                    ),
-                    leading: Radio(
-                      value: ClientRole.Broadcaster,
-                      groupValue: _role,
-                      onChanged: (ClientRole value) {
-                        setState(() {
-                          _role = value;
-                        });
-                      },
-                    ),
-                  ),
-                  ListTile(
-                    // title: Text(ClientRole.Audience.toString()),
-                    title: Text(
-                      "Join a meeting",
-                      style: TextStyle(
-                          fontFamily: 'Gotham', color: Hexcolor('#5C6178')),
-                    ),
-                    leading: Radio(
-                      value: ClientRole.Audience,
-                      groupValue: _role,
-                      onChanged: (ClientRole value) {
-                        setState(() {
-                          _role = value;
-                        });
-                      },
-                    ),
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Row(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Row(
                   children: <Widget>[
                     Expanded(
-                      child: RaisedButton(
-                        onPressed: onJoin,
-                        child: Text(
-                          'Let\'s go!',
-                          style: TextStyle(fontFamily: 'Poppins'),
+                        child: TextField(
+                      style: TextStyle(
+                          color: Hexcolor('#5C6178'), fontFamily: 'Gotham'),
+                      controller: _channelController,
+                      decoration: InputDecoration(
+                        labelStyle: TextStyle(
+                            color: Hexcolor('#5C6178'), fontFamily: 'Gotham'),
+                        hintStyle: TextStyle(
+                            color: Hexcolor('#5C6178'), fontFamily: 'Gotham'),
+                        errorStyle:
+                            TextStyle(color: Colors.red, fontFamily: 'Gotham'),
+                        errorText: _validateError ? 'ID is mandatory' : null,
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide(width: 1),
                         ),
-                        color: Colors.white,
-                        textColor: Colors.black,
+                        hintText: 'Enter Team ID',
                       ),
-                    ),
+                    ))
                   ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Kindly contact your administrator for an ID if you are joining as an attendee.",
-                  style: TextStyle(
-                      color: Hexcolor('#5C6178'),
-                      fontSize: 14,
-                      fontStyle: FontStyle.italic,
-                      fontFamily: 'Gotham'),
+                Column(
+                  children: [
+                    ListTile(
+                      //title: Text(ClientRole.Broadcaster.toString()),
+                      title: Text(
+                        "Start a call",
+                        style: TextStyle(
+                            fontFamily: 'Gotham', color: Hexcolor('#5C6178')),
+                      ),
+                      leading: Radio(
+                        value: ClientRole.Broadcaster,
+                        groupValue: _role,
+                        onChanged: (ClientRole value) {
+                          setState(() {
+                            _role = value;
+                          });
+                        },
+                      ),
+                    ),
+                    ListTile(
+                      // title: Text(ClientRole.Audience.toString()),
+                      title: Text(
+                        "Join a meeting",
+                        style: TextStyle(
+                            fontFamily: 'Gotham', color: Hexcolor('#5C6178')),
+                      ),
+                      leading: Radio(
+                        value: ClientRole.Audience,
+                        groupValue: _role,
+                        onChanged: (ClientRole value) {
+                          setState(() {
+                            _role = value;
+                          });
+                        },
+                      ),
+                    )
+                  ],
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: RaisedButton(
+                          onPressed: onJoin,
+                          child: Text(
+                            'Let\'s go!',
+                            style: TextStyle(fontFamily: 'Poppins'),
+                          ),
+                          color: Colors.white,
+                          textColor: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Kindly contact your administrator for an ID if you are joining as an attendee.",
+                    style: TextStyle(
+                        color: Hexcolor('#5C6178'),
+                        fontSize: 14,
+                        fontStyle: FontStyle.italic,
+                        fontFamily: 'Gotham'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
